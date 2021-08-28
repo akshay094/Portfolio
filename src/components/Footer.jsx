d3600d8e3193cd708cb3fc6711dbf7cc133a8a96
Akshay kanojia <akanojia.47@gmail.com>
2021-08-28 18:39:53 +0530

    Footer design repaired

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -13,15 +13,15 @@ const Footer = () => {
           <Col md={10} className="mx-auto mt-4 p-3 h3 fw-bold">
             Wanna Connect ?
           </Col>
-          <Col xs={4} md={4} lg={4} className="mx-auto mb-4">
-            <Row className="text-center text-light fw-bold">
+          <Col xs={10} md={10} lg={10} className="mx-auto mb-4">
+            <Row className=" text-light fw-bold">
               <Col xs={12} md={12} lg={12} >
-                <a href="https://discord.gg/AabR3qvJZd" className="text-light fw-bold text-decoration-none mx-3"><Discord className="m-2" /></a>
+                <a href="https://discord.gg/AabR3qvJZd" className=" text-center mx-3"><Discord className="m-2" /></a>
               </Col>
               <Col xs={12} md={12} lg={12}>
-                <a href="https://github.com/akshay094" className="text-light fw-bold text-decoration-none mx-3"><GitHub className="img-fluid m-2" /></a>
+                <a href="https://github.com/akshay094" className=" mx-3"><GitHub className=" m-2" /></a>
 
-                <a href="https://www.linkedin.com/in/akshay-kanojia-2a019bb3/" className="text-light fw-bold text-decoration-none mx-3"><LinkedIn className="img-fluid m-2" /></a>
+                <a href="https://www.linkedin.com/in/akshay-kanojia-2a019bb3/" className=" mx-3"><LinkedIn className="m-2" /></a>
               </Col>
             </Row>
           </Col>
